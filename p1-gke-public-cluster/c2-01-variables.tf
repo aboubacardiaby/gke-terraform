e04665e62328387241d5e84b4cf218d367170531
abou diaby <aboubacardiaby@informatixs.onmicrosoft.com>
2025-01-16 12:03:27 -0600

    some changes

--- p1-gke-public-cluster/c2-01-variables.tf
+++ p1-gke-public-cluster/c2-01-variables.tf
@@ -2,35 +2,35 @@
 # GCP Project
 variable "gcp_project" {
   description = "Project in which GCP Resources to be created"
-  type = string
-  default = "kdaida123"
+  type        = string
+  default     = "fodekaba2024"
 }
 
 # GCP Region
 variable "gcp_region1" {
   description = "Region in which GCP Resources to be created"
-  type = string
-  default = "us-east1"
+  type        = string
+  default     = "us-east1"
 }
 
 # GCP Compute Engine Machine Type
 variable "machine_type" {
   description = "Compute Engine Machine Type"
-  type = string
-  default = "e2-small"
+  type        = string
+  default     = "e2-small"
 }
 
 
 # Environment Variable
 variable "environment" {
   description = "Environment Variable used as a prefix"
-  type = string
-  default = "dev"
+  type        = string
+  default     = "dev"
 }
 
 # Business Division
 variable "business_divsion" {
   description = "Business Division in the large organization this Infrastructure belongs"
-  type = string
-  default = "sap"
+  type        = string
+  default     = "sap"
 }
--- p1-gke-public-cluster/terraform.tfvars
+++ p1-gke-public-cluster/terraform.tfvars
@@ -1,4 +1,4 @@
-gcp_project      = "workingpaper01"
+gcp_project      = "fodekaba2024"
 gcp_region1      = "us-central1"
 machine_type     = "e2-medium"
 environment      = "dev"
